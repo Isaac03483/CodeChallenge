21b24219aac1bcf6350acc8de41b7f1f3c732a8c
Isaac03483 <levihernandez2501@gmail.com>
2025-02-07 13:01:31 -0600

    Mejorando codigo

--- CodeChallenge/src/main/java/org/example/App.java
+++ CodeChallenge/src/main/java/org/example/App.java
@@ -16,47 +16,42 @@ public class App {
      * Complejidad del algoritmo: O(n + m + p)
      * 
      */
-    public boolean checkAnagrama(String word1, String word2) {  // O(n) + O(p) + O(m) = O(n + m + p)
+    public boolean checkAnagrama(String word1, String word2) {  // O(n) + O(m) = O(n + m) = O(n)
         Map<Character, Integer> mapWord1 = new HashMap<>();
         Map<Character, Integer> mapWord2 = new HashMap<>();
 
         if(word1 == null || word2 == null) return false;
-
         char[] arrayWord1 = word1.toCharArray();
         char[] arrayWord2 = word2.toCharArray();
 
-        for(char charWord: arrayWord1) {                        // O(n)
-            Character character = Character.valueOf(charWord);
-            Integer value = mapWord1.get(character);
+        if(arrayWord1.length != arrayWord2.length) return false;
 
-            if (value == null) {
-                mapWord1.put(character, 1);
+        for(int i = 0; i < arrayWord1.length; i++) {                        // O(n)
+            Character character1 = Character.valueOf(arrayWord1[i]);
+            Integer value1 = mapWord1.get(character1);
+
+            
+            if (value1 == null) {
+                mapWord1.put(character1, 1);
             } else {
-                mapWord1.computeIfPresent(character, (k, v) -> v + 1);
+                mapWord1.computeIfPresent(character1, (k, v) -> v + 1);
             }
 
-        }
+            Character character2 = Character.valueOf(arrayWord2[i]);
+            Integer value2 = mapWord2.get(character2);
 
-        for(char charWord: arrayWord2) {                        // O(m)
-            Character character = Character.valueOf(charWord);
-            Integer value = mapWord2.get(character);
-
-            if (value == null) {
-                mapWord2.put(character, 1);
+            if (value2 == null) {
+                mapWord2.put(character2, 1);
             } else {
-                mapWord2.computeIfPresent(character, (k, v) -> v + 1);
+                mapWord2.computeIfPresent(character2, (k, v) -> v + 1);
             }
-
         }
 
         if(mapWord1.size() != mapWord2.size()) return false;
 
-        for(var keyValue: mapWord1.entrySet()) {                // O(p)
-            Integer num = mapWord2.get(keyValue.getKey());
-            if(num == null) return false;
-            if(num != keyValue.getValue()) return false;
-        }
+        // { a: 1, p: 2, s: 3}
+        return mapWord1.entrySet().stream().allMatch(v -> v.getValue() == mapWord2.get(v.getKey()));
 
-        return true;
+        // return true;
     }
 }
